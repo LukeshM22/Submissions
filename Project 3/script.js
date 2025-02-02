33b8b6a02d9f848708a3e89056419fc5c6f27d31
LukeshM22 <118557176+LukeshM22@users.noreply.github.com>
2025-02-03 00:23:56 +0530

    Add files via upload

---
+++ Project 3/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const addToCartButton = document.getElementById("add-to-cart");
+
+    addToCartButton.addEventListener("click", function() {
+        alert("Product added to cart!");
+    });
+});
